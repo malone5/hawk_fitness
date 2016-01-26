b02db612e5bad767f093631407c6dbbfd175cfea
shujar <raheelshuja@gmail.com>
2016-01-26 12:51:30 -0500

    made a dynamic variable in header for custom css
    added a custom variable for css link in the header, so that each page can have a its own stylesheet.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>HawkFitness Prototype</title>
+		<title><?php echo $title;?></title>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
@@ -9,9 +9,11 @@
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" >
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" ></script>
-		<link rel="stylesheet" href="<?php echo base_url('assets/css')?>/main.css" >
 
+		<!--custom page css-->
+		<link rel="stylesheet" href="<?php echo base_url('assets/css').'/'.$css; ?>" >
 	</head>
+	
 	<body>
 		<div class="row">
 			<div class="jumbotron text-center">
