7502a64f1acdc8d0219e8265ae322e909f98019f
Sean Malone <malonespm@gmail.com>
2016-01-29 22:00:08 -0500

    deleted some bloat in the admin dash components. Upadte admin headers and footers

--- application/views/templates/admin_footer.php
+++ application/views/templates/admin_footer.php
@@ -4,10 +4,14 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="<?php echo base_url('assets/admin/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+   
 
+    <script src="<?php echo base_url('assets/admin/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+    <script src=" https://code.jquery.com/jquery-2.2.0.min.js"></script>
     <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url('assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <!-- <script src="<?php echo base_url('assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script> -->
 
     <!-- Metis Menu Plugin JavaScript -->
     <script src="<?php echo base_url('assets/admin/bower_components/metisMenu/dist/metisMenu.min.js') ?>"></script>
