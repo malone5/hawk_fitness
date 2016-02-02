011e561cffed0ed624f9f85bf684ec3ecdad19bb
ebrock <ebrockk@gmail.com>
2016-02-02 00:21:16 -0500

    Footer init commit

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,12 +1,16 @@
-		<div class="row text-center">
-
+		<div class="row text-left">
 			<hr>
-			<em>HawkFitness&copy; 2015</em>
-
-			<!-- <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-			<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.jss"></script> -->
-
+			<div class="col-md-6">
+				<em>HawkFitness&copy; 2015</em>
+			</div>
+		</div>
+		<div class = "row text-right">
+			<div class="col-md-6">
+				<input type="button" class="btn btn-primary" value="Login" onclick="location.href='<?php echo base_url();?>login'"></input>
+			</div>
 		</div>
 
 	</body>
 </html>
+<!-- <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+			<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.jss"></script> -->
