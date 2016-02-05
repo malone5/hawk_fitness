01e602a78184573385cddd94b509b8db8f21889b
Eric Brock <Eric Brock>
2016-02-05 00:17:15 -0500

    Pulled footer down, added links, spacing.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,15 +1,22 @@
-		<div class="row text-left">
-			<hr>
-			<div class="col-md-6">
+		<div class="navbar navbar-fixed-bottom">
+			<div class="row text-center">
+				<hr>
+				<div class="col-md-4"></div>
+				<div class="col-md-4">
+					<strong><a href="http://www.monmouth.edu/" title="Monmouth University">Monmouth University</a></strong>
+					 / 
+					<strong><a href="http://www.monmouthhawks.com/ViewArticle.dbml?ATCLID=714830" title="Monmouth University Fitness Center">Fitness Center</a></strong>
+					 / 
+					<strong><a href="http://www.monmouthhawks.com/ViewArticle.dbml?ATCLID=722884" title="Monmouth University Intramurals">Intramurals</a></strong>
+				</div>
+				<div class="col-md-4">
+					<input type="button" class="btn btn-primary" value="Administration" onclick="location.href='<?php echo base_url();?>login'"></input>
+				</div>
+			</div>
+			<div class="row text-center">
 				<em>HawkFitness&copy; 2015</em>
 			</div>
 		</div>
-		<div class = "row text-right">
-			<div class="col-md-6">
-				<input type="button" class="btn btn-primary" value="Login" onclick="location.href='<?php echo base_url();?>login'"></input>
-			</div>
-		</div>
-
 	</body>
 </html>
 <!-- <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
