e7c19c721328495c65e1ed79505f1d9f02e1e937
Sean Malone <malonespm@gmail.com>
2016-02-07 17:53:33 -0500

    updated DB file. Changes class_type to VARCHAR.

--- db/hawk_fitness.sql
+++ db/hawk_fitness.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2016 at 07:35 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Feb 07, 2016 at 11:52 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `hawk_fitness`
@@ -44,22 +44,20 @@ CREATE TABLE IF NOT EXISTS `attendees` (
 --
 
 CREATE TABLE IF NOT EXISTS `class_type` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `id` int(4) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `description` varchar(500) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `class_type`
 --
 
 INSERT INTO `class_type` (`id`, `name`, `description`) VALUES
-(1, 'Yoga', ''),
-(2, 'Pilates', ''),
-(3, 'Butt Buster', ''),
-(4, 'Kickboxing', ''),
-(5, 'Zumba', '');
+(1, 'Yoga', 'This is the Yoga description'),
+(3, 'Butt Buster', 'This is ButtBuster description'),
+(4, 'Kickboxing', 'This is Kickboxing description'),
+(5, 'Zumba', 'This is Zumba description');
 
 -- --------------------------------------------------------
 
@@ -68,28 +66,25 @@ INSERT INTO `class_type` (`id`, `name`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `fit_classes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `slug` varchar(128) NOT NULL,
-  `class_type` int(4) NOT NULL,
+  `id` int(11) NOT NULL,
+  `class_type` varchar(20) NOT NULL,
   `instructor` varchar(30) NOT NULL,
   `location` varchar(20) NOT NULL,
   `start_time` varchar(8) NOT NULL,
-  `date` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+  `date` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `fit_classes`
 --
 
-INSERT INTO `fit_classes` (`id`, `slug`, `class_type`, `instructor`, `location`, `start_time`, `date`) VALUES
-(14, '', 3, 'Sean', 'Boylin Gym', '8:30 PM', '2015-10-14'),
-(15, '', 4, 'Miles Autsin', 'Boylin Gym', '9:30om', '2015-10-31'),
-(16, '', 2, 'Mrs. Puff', 'Under the Boardwalk', '12:00pm', '2015-11-04'),
-(17, '', 2, 'Ken', 'MAC', '3:30pm', '2015-11-27'),
-(18, '', 1, 'Sir Mix-A-Lot', 'Hollywood', '9:00pm', '2016-02-02'),
-(19, '', 1, 'Guru Ghulab', 'Yoga Studio', '6:30', '2016-02-01'),
-(20, '', 4, 'Shady', 'Detroit', '9:30am', '2016-02-01');
+INSERT INTO `fit_classes` (`id`, `class_type`, `instructor`, `location`, `start_time`, `date`) VALUES
+(14, 'Butt Buster', 'Sean', 'Boylin Gym', '8:30 PM', '2015-10-14'),
+(15, 'Zumba', 'Miles Autsin', 'Boylin Gym', '9:30om', '2015-10-31'),
+(17, 'Kickboxing', 'Ken', 'MAC', '3:30pm', '2015-11-27'),
+(18, 'Yoga', 'Sir Mix-A-Lot', 'Hollywood', '9:00pm', '2016-02-02'),
+(19, 'Butt Buster', 'Guru Ghulab', 'Yoga Studio', '6:30', '2016-02-01'),
+(27, 'Kickboxing', 'test instruct changed to kickb', 'testt location updat', '8:00pm', '2016-02-18');
 
 -- --------------------------------------------------------
 
@@ -98,15 +93,14 @@ INSERT INTO `fit_classes` (`id`, `slug`, `class_type`, `instructor`, `location`,
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `id` tinyint(4) NOT NULL,
   `uname` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `pword` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `fname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `lname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `role` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+  `role` varchar(10) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `users`
@@ -115,6 +109,47 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `uname`, `pword`, `fname`, `lname`, `email`, `role`) VALUES
 (1, 'admin1', 'secrete', 'Jon', 'Cascone', 'nothing@nothing.com', 'admin');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `class_type`
+--
+ALTER TABLE `class_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `fit_classes`
+--
+ALTER TABLE `fit_classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `class_type`
+--
+ALTER TABLE `class_type`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `fit_classes`
+--
+ALTER TABLE `fit_classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
