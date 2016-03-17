b8ec0d846d076f4d3439925aaabd57464b7bb144
Sean Malone <malonespm@gmail.com>
2016-03-17 03:20:51 -0400

    removed commented out code in checkin

--- application/views/manage/checkin.php
+++ application/views/manage/checkin.php
@@ -33,14 +33,3 @@
         <?php }
       }?>
 </div>
-
-
-<!-- 
-
-<span class="class-name"><?php echo $class['class_type']?></span>
-<div class="class-information"> <?php echo $class['instructor']?></div>
-<div class="class-information"> <?php echo $class['location']?></div>
-<div class="class-information"> <?php echo $class['start_time']?></div>
-<span class="class-link"><a href="<?php echo site_url('checkin').'/'.$class['id'];?>">check in</a></span> 
-
--->
