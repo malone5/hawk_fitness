405c6a4e6da00f177399fc421c0e96395cb5bde1
ebrock <ebrockk@gmail.com>
2016-04-20 11:12:56 -0400

    Hot fix for Footer. Added photographer credit

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,14 +2,23 @@
 					<div class="container-fluid foot">
 						<div class="row text-center">
 							<hr>
-							<div class="col-lg-4"></div>
+							<div class="col-lg-4" style="font-size: 85%; color:#FFFFFF">
+								<p><i>Background by</i>
+								</br>
+								<i>Robert Faulkner Studios</i></a>
+							</p>
+							</div>
 							<div class="col-lg-4">
 								<div class="links">
-	                  <a href="http://www.monmouth.edu/" title="Monmouth University"><strong>Monmouth University</strong></a>
+	                  				<a href="http://www.monmouth.edu/" title="Monmouth University"><strong>Monmouth University</strong></a>
 									 /
-	                  <a href="http://www.monmouthhawks.com/ViewArticle.dbml?ATCLID=714830" title="Monmouth University Fitness Center"><strong>Fitness Center</strong></a>
+	                  				<a href="http://www.monmouthhawks.com/ViewArticle.dbml?ATCLID=714830" title="Monmouth University Fitness Center"><strong>Fitness Center</strong></a>
 									 /
-										<a href="http://www.monmouthhawks.com/ViewArticle.dbml?ATCLID=722884" title="Monmouth University Intramurals"><strong>Intramurals</strong></a>
+									<a href="http://www.monmouthhawks.com/ViewArticle.dbml?ATCLID=722884" title="Monmouth University Intramurals"><strong>Intramurals</strong></a>
+									</br>
+									<div class="row text-center copy-right">
+										<p class="copy">Monmouth University Fitness&copy; 2016</p>
+									</div>
 								</div>
 							</div>
 							<div class="col-lg-4">
@@ -18,9 +27,6 @@
 								<!--Logout Button-->
 								<?php if(isset($logout_btn)) echo $logout_btn;?>
 							</div>
-						</div>
-						<div class="row text-center copy-right">
-							<p class="copy">HawkFitness&copy; 2015</p>
 						</div>
 					</div>
 				</div>
